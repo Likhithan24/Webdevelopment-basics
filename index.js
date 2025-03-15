4be990537346b2920da05b5800faf79d8ba219a1
Likhithan24 <gowdalikithan5@gmail.com>
2025-03-15 10:58:48 +0530

    Add files via upload
    Sir,we have done these Web developments projects in Web Training Class

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("hello world")
+    // let name = 'likhitha';
+    // let age = 18;
+    // console.log(name);
+    // console.log(age);
+
+// let firstname = 'likhitha';
+// let lastname = 'gowda';
+// name = firstname + "" + lastname
+// console.log(name);
+
+// let name = "likhitha";
+// let age = 19;
+// let isApproved = false;
+// let firstname = undefined;
+// let selectedcolor = null;
+
+// let person = {
+//     name: "rakshi",
+//     age: 20
+// };
+// console.log(person);
+// person.name = "likitha";
+// person["age"] = 19;
+// console.log(person);
+
+// let selectedfruits = ['apple', 'guva', 'cherry']
+// selectedfruits[3] = 'grapes'
+// console.log(selectedfruits)
+// console.log(selectedfruits.length)
+
+// function square(number) {
+//     return number * number;
+// }
+
+// function cube(number) {
+//     return number * number * number;
+// }
+// console.log(square(5));
+// console.log(cube(5));
+
+window.alert('your 50% data is over')
+
+document.getElementById("myh1").textcontent = "welcome"
+name;
+document.getElementById("myp").textcontent = "I like Javascript";
